bea77cb002d2bea4d0724cb9ef7a420782023dd9
Tigist Ashine <tigistashine22@gmail.com>
2019-08-13 09:51:48 +0300

    add

---
+++ web based hospital managmenty system/Hospital1/doctortg.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, td, th {
+    border: 1px solid black;
+}
+?
+.table1 {
+    border-collapse: collapse;
+    width: 50%;
+    margin-left: inherit;
+    text-align: center;
+    justify-content: center;
+   font-family: sans-serif;
+   font-size: 250%;
+}
+table {
+    margin-right: 0px;
+    margin-left: 450px;
+    margin-top: 20px;
+    margin-bottom: 100px;
+  
+th {
+    height: 0px;
+    width:100px;
+    
+}
+</style>
+</head>
+<body>
+ <table class="table">
+  <thead>
+    <div class="table">
+        
+        <center>
+      <h3>MRN/Phone No<input type="text"  name="MRN/Phone No"><button>search</button></h3>
+    </form><br><br></div></center>
+     
+  <tr>
+    <th>Number</th>
+    <th>MRN</th>
+    <th>Full name</th>
+    <th>Date</th>
+  </tr>
+  <tr>
+    <td>  <input type="checkbox">1</td>
+    <td><input type="checkbox">1234</td>
+    <td><input type="checkbox">Fikr</td>
+    <td><input type="checkbox">3/7/2011 </td>
+    <td><button>view</button><button>send lab test</button><button>see lab result</button></td>
+  </tr>
+  <tr>
+    <td><input type="checkbox">2</td>
+    <td><input type="checkbox">123</td>
+     <td><input type="checkbox">Tg</td>
+    <td><input type="checkbox">12/7/2011</td>
+    <td><button>view</button><button>send lab test</button><button>see lab result</button></td>
+  </tr>
+  <tr>
+    <td><input type="checkbox">3</td>
+    <td><input type="checkbox">1234</td>
+    <td><input type="checkbox">Mesi</td>
+    <td><input type="checkbox">1/7/2011</td>
+    <td><button>view</button><button>send lab test</button><button>see lab result</button></td>
+    
+  </tr>
+  <tr>
+    <td><input type="checkbox">4</td>
+    <td><input type="checkbox">1234</td>
+    <td><input type="checkbox">ledu</td>
+    <td><input type="checkbox">3/7/2011</td>
+    <td><button>view</button><button>send lab test</button><button>see lab result</button></td>
+  </tr>
+  
+  <tr>
+    <td><input type="checkbox">5</td>
+    <td><input type="checkbox">1234</td>
+    <td><input type="checkbox">leli</td>
+    <td><input type="checkbox">16/7/2011</td>   
+    <td><button>view</button><button>send lab test</button><button>see lab result</button></td>
+  </tr>
+</table>
+    
+  
+</body>
+</html>
