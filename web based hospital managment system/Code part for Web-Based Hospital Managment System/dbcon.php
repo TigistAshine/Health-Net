6d54909bf3a9f5cb20f1008332cf810dc842dde5
Tigist Ashine <tigistashine22@gmail.com>
2019-08-25 21:40:55 +0300

    add

--- web based hospital managment system/Code part for Web-Based Hospital Managment System/dbcon.php
+++
@@ -1,8 +0,0 @@
-<?php
-class connectionClass extends mysqli{
-    private $host="localhost",$password="",$username="root",$dbName='hospital';
-    public $con;
-    function __construct(){
-        $this->con=$this->connect($this->host,$this->username,$this->password,$this->dbName);
-    }
-}
